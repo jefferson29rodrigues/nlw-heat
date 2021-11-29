c99121a1a594e13ba0ee13440e508c70675cfe42
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-11-29 15:20:23 -0300

    criando componentes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
+import styles from "./App.module.scss";
+import { LoginBox } from "./components/LoginBox";
+import { MessageList } from "./components/MessageList";
 
 export function App() {
 
   return (
-    <h1>Hello NLW Heat!</h1>
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
   )
 }
