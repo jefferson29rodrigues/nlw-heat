33a85228fa92446140127184eb870c6d52350f47
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-12-02 08:03:29 -0300

    pegando urlWithoutCode e githubCode

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { VscGithubInverted } from "react-icons/vsc"
 
 import styles from "./styles.module.scss";
@@ -5,6 +6,18 @@ import styles from "./styles.module.scss";
 export function LoginBox() {
 
     const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=d9ef69cd2acb9cd8e224`;
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code=');
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=');
+
+            console.log({ urlWithoutCode, githubCode });
+        }
+
+    }, []);
 
     return (
         <div className={styles.loginBoxWrapper}>
